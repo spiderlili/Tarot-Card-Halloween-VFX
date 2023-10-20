d70020620b19566c66ae29131bbbb6b16cc82d36
Jing Tan <jtan1993@gmail.com>
2023-10-20 15:29:08 +0100

    add turnaround script

--- Assets/StencilExamples/StencilMask/Scripts/TurnAroundScript.cs
+++ Assets/StencilExamples/StencilMask/Scripts/TurnAroundScript.cs
@@ -2,14 +2,34 @@
 
 public class TurnAroundScript : MonoBehaviour
 {
-
-
     public float turnSpeed = 40f;
-    // Use this for initialization
+    public RotationAxis rotationAxis = RotationAxis.Y; // Default to rotating around the Y-axis
 
-    // Update is called once per frame
+    public enum RotationAxis
+    {
+        X,
+        Y,
+        Z
+    }
+
     void Update()
     {
-        transform.Rotate(0, Time.deltaTime * turnSpeed, 0);
+        Vector3 rotationVector = Vector3.zero;
+
+        // Set the rotationVector based on the selected axis
+        switch (rotationAxis)
+        {
+            case RotationAxis.X:
+                rotationVector = new Vector3(1, 0, 0);
+                break;
+            case RotationAxis.Y:
+                rotationVector = new Vector3(0, 1, 0);
+                break;
+            case RotationAxis.Z:
+                rotationVector = new Vector3(0, 0, 1);
+                break;
+        }
+        
+        transform.Rotate(rotationVector * Time.deltaTime * turnSpeed);
     }
 }
